4c24823f5242c24e79a3c42828f456d2c3115683
rlrin <32531841+rlrin@users.noreply.github.com>
2021-02-25 17:56:08 +0800

    Update diy-2.sh

--- build/Lede_nanopi_r2s/diy-2.sh
+++ build/Lede_nanopi_r2s/diy-2.sh
@@ -32,5 +32,5 @@ sed -i 's/"TTYD 终端"/"命令窗"/g' package/lean/luci-app-ttyd/po/zh-cn/termi
 sed -i 's/"USB 打印服务器"/"打印服务"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 sed -i 's/"网络存储"/"存储"/g' package/lean/luci-app-usb-printer/po/zh-cn/usb-printer.po
 sed -i 's/"Web 管理"/"Web"/g' package/lean/luci-app-webadmin/po/zh-cn/webadmin.po
-sed -i 's/"管理权"/"改密码"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
+sed -i 's/"管理权"/"后台权限"/g' feeds/luci/modules/luci-base/po/zh-cn/base.po
 sed -i 's/"带宽监控"/"监视"/g' feeds/luci/applications/luci-app-nlbwmon/po/zh-cn/nlbwmon.po
